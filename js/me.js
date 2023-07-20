4b323c84715df1ca65bfaf10b324be0963856cea
Daxa28 <osadasha201628@gmail.com>
2023-07-20 19:29:00 +0300

    first commit

---
+++ js/me.js
@@ -0,0 +1,43 @@
+// const delay = ms => {
+//     return new Promise (r => setTimeout(() => r(), ms))
+// }
+
+// // delay(2000).then(() => console.log('dasha'))
+
+// const url = 'https://jsonplaceholder.typicode.com/todos'
+
+
+// function fetchTodos() {
+//     console.log('Fetch todo started..')
+//     return delay(2000)
+//         .then (() => fetch(url))
+//         .then(response => response.json())
+// }
+
+// fetchTodos()
+//     .then(data => {
+//         console.log('Data:', data)
+//     })
+//     .catch(e => console.error(e))
+
+
+// async function fetchAsyncTodos() {
+//     try {
+//         console.log('Fetch todo started..')
+//         await delay(2000)
+//         const response = await fetch(url)
+//         const data = await response.json()
+//         console.log('Data:', data)
+//     } catch (e) {
+//         console.error(e)
+//     } finally {
+//         console.log('Hello')
+//     }
+
+// }
+// fetchAsyncTodos();
+
+let a = 6
+let b = 6 
+
+console.log(Object.is(a, b))
